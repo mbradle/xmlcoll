a86ece574cf8c8d66bb3d7fb13f896a7e1be6984
Bradley Meyer <mbradle@clemson.edu>
2022-10-08 14:07:00 -0400

    All multiple xml catalogs.

--- doc/source/changelog.rst
+++ doc/source/changelog.rst
@@ -4,6 +4,13 @@ Changelog
 All notable changes to this project will be documented in this file.  This
 project adheres to `Semantic Versioning <http://semver.org/spec/v2.0.0.html>`_.
 
+Version 1.2.0
+-------------
+
+Internal:
+
+  * Multiple xml catalogs are now allowed within python.
+
 Version 1.1.0
 -------------
 
--- xmlcoll/__init__.py
+++ xmlcoll/__init__.py
@@ -3,9 +3,12 @@ A package of python routines to work with data in XML format of samples.
 """
 import os
 
-os.environ["XML_CATALOG_FILES"] = os.path.join(
-    os.path.dirname(__file__), "xsd_pub/catalog"
-)
+xml_catalog = os.path.join(os.path.dirname(__file__), "xsd_pub/catalog")
+
+if "XML_CATALOG_FILES" in os.environ:
+    os.environ["XML_CATALOG_FILES"] += " " + xml_catalog
+else:
+    os.environ["XML_CATALOG_FILES"] = xml_catalog
 
 from xmlcoll.coll import *
 from xmlcoll.base import *
