b96c8980fee7ffc92944553dfb326c06c1e5693c
Bradley Meyer <mbradle@clemson.edu>
2024-11-23 09:05:10 -0500

    Fix citation file.  Add version check.

--- build.sh
+++ build.sh
@@ -7,3 +7,17 @@ git clone https://bitbucket.org/mbradle/xmlcoll_xsd.git xsd_pub
 cd ..
 python -m pip install --upgrade build
 python -m build
+
+echo ""
+echo "All version numbers must be the same:"
+echo ""
+
+grep version xmlcoll/__about__.py | grep -v ","
+grep version CITATION.cff | grep -v "cff-version"
+grep Version doc/source/changelog.rst | grep -v Versioning | head -1
+
+echo ""
+echo "Check the release date:"
+echo ""
+grep date CITATION.cff
+echo ""
